3c8ab3dcf2671657ef1a32335a75bb9272e7ba84
Miguel Ángel Sánchez Chordi <mangel.snc@gmail.com>
2021-11-26 17:28:11 +0100

    Add RowIterator

---
+++ src/RowIterator.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Secture\Sudoku;
+
+final class RowIterator
+{
+    private int $currentIteration;
+
+    public function __construct(private array $sudoku)
+    {
+        $this->currentIteration = 0;
+    }
+
+    public function next(): ?array
+    {
+        if ($this->currentIteration >= count($this->sudoku)) {
+            return null;
+        }
+
+        return $this->sudoku[$this->currentIteration++];
+    }
+}
---
+++ test/RowIteratorTest.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Secture\Sudoku\RowIterator;
+
+final class RowIteratorTest extends TestCase
+{
+    const SUDOKU_LINE = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    /** @test  */
+    public function itShouldReturnNextRow()
+    {
+        $sudoku = [
+            self::SUDOKU_LINE,
+            array_reverse(self::SUDOKU_LINE),
+            self::SUDOKU_LINE,
+        ];
+        $iterator = new RowIterator($sudoku);
+
+        $this->assertEquals(self::SUDOKU_LINE, $iterator->next());
+        $this->assertEquals(array_reverse(self::SUDOKU_LINE), $iterator->next());
+        $this->assertEquals(self::SUDOKU_LINE, $iterator->next());
+    }
+
+    /** @test  */
+    public function itShouldReturnNullIfTheresNoNextRow()
+    {
+        $sudoku = [
+            self::SUDOKU_LINE,
+            array_reverse(self::SUDOKU_LINE),
+            self::SUDOKU_LINE,
+        ];
+        $iterator = new RowIterator($sudoku);
+
+        $this->assertEquals(self::SUDOKU_LINE, $iterator->next());
+        $this->assertEquals(array_reverse(self::SUDOKU_LINE), $iterator->next());
+        $this->assertEquals(self::SUDOKU_LINE, $iterator->next());
+        $this->assertNull($iterator->next());
+    }
+}
